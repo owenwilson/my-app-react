87ce3f4a793336510a7814014af7fdb4bffd9943
owenwilson <owenwilson76467963@gmail.com>
2018-01-15 01:49:53 -0400

    new name import EstadoUsuario

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 //import './App.css';
 
-import Title from './components/Title';
+import EstadoUsuario from './components/EstadoUsuario';
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
       <header className='App-header'>
-      <Title />
+      <EstadoUsuario />
       </header>
       </div>
     );
